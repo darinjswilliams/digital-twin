fb1f281eaf6fad779b92e1e4cff1f194f4476550
Darin Williams <darinjswilliams@icloud.com>
2025-10-31 12:19:24 -0500

    Update Page Layout

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
             Deploy your Digital Twin to the cloud
           </p>
 
-          <div className="h-[800px]">
+          <div className="h-[1000px]">
             <Twin />
           </div>
 
